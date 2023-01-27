c212ea1071774627bf6c6a367f714c71e2a16170
Overflow922 <genius922@yandex.ru>
2023-01-27 21:14:48 +0300

    quiz + options

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -7,7 +7,7 @@
 // - Enums
 
 // Let's build a little machine in form of a function.
-// As input, we're going to give a list of strings and commands. These commands
+// As input, we're going to give a list of strings and commands .These commands
 // determine what action is going to be applied to the string. It can either be:
 // - Uppercase the string
 // - Trim the string
@@ -18,7 +18,22 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
+mod my_module {
+    use super::Command;
+
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+        for (string, command) in input.iter() {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(c) => output.push(string.to_owned() + &"bar".repeat(*c))
+            }
+        }
+        println!("{:?}", &output);
+        output
+    }
+}
 
 pub enum Command {
     Uppercase,
@@ -26,24 +41,10 @@ pub enum Command {
     Append(usize),
 }
 
-mod my_module {
-    use super::Command;
-
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
-        }
-        output
-    }
-}
-
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
