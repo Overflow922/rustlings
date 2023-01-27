3823735f2d265eaabfa85a11568187fd69714d11
Overflow922 <genius922@yandex.ru>
2023-01-27 20:27:45 +0300

    hash maps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -16,8 +16,6 @@
 
 // I AM NOT DONE
 
-use std::collections::HashMap;
-
 // A structure to store team name and its goal details.
 struct Team {
     name: String,
@@ -40,6 +38,23 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+
+        let team = scores.entry(team_1_name.to_owned()).or_insert(Team {
+            name: team_1_name,
+            goals_scored: 0,
+            goals_conceded: 0
+        });
+        team.goals_scored += team_1_score;
+        team.goals_conceded += team_2_score;
+
+        let team = scores.entry(team_2_name.to_owned()).or_insert(Team {
+            name: team_2_name,
+            goals_scored: 0,
+            goals_conceded: 0
+        });
+        team.goals_scored += team_2_score;
+        team.goals_conceded += team_1_score;
     }
     scores
 }
